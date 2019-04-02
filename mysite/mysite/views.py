98c14599ada75547b158fe30e655bbb899aa6d0a
Carlton Welch <Carl@Carltons-MacBook-Air.local>
2019-04-02 19:56:30 -0400

    cancel button for login redirect

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.views import generic
 
+from advising.models import Advising
+
 def Home(request):
+    
+    if request.user.is_authenticated:
+
+        #gets advising form status by checking Advising model for EMPLID match
+        adv_form = Advising.objects.filter(author = request.user.EMPLID)
+        adv_status = 0
+
+        if len(adv_form) != 0:
+            print("adv_check != 0")
+            adv_form = list(adv_form)[0]
+            adv_status = adv_form.status
+
+        return render(request, 'home.html', {
+            'adv_status' : adv_status,
+            'adv_form' : adv_form
+        })
+    
     return render(request, 'home.html')
