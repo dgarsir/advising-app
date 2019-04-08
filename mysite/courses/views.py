d9c0b3e62943069d3133e71ab5e3ca6c75e67036
dgarsir <welch.carlton@gmail.com>
2019-04-08 14:00:17 -0400

    courses working, all features functional

--- mysite/courses/views.py
+++ mysite/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import AddClassForms
+from .models import Courses
 
 # Create your views here.
 def courses_add(request):
@@ -12,3 +13,25 @@ def courses_add(request):
     else:
         form = AddClassForms()
     return render(request, 'courses_add.html', {'form': form})
+
+def view_courses(request):
+    print(request.POST)
+    queryset=Courses.objects.all()
+    context ={
+        "object_list": queryset,
+        "title": "list"
+    }
+    '''if request.method == "POST":
+        for obj in queryset:
+            print(obj.courses_name)
+            print(obj.teacher)
+        return redirect('home')'''
+    return render(request, 'view_courses.html',context)
+
+
+def del_course(request):
+    course = list(Courses.objects.all())
+    if request.method == "POST":
+        Courses.objects.filter(pk = request.POST.get('to-delete')).delete()
+    return render(request,'del_course.html', {'course': course})
+
