290e164e0814680b64afd0b2b99730ab767d7106
dgarsir <welch.carlton@gmail.com>
2019-04-09 19:00:17 -0400

    removed mysite

--- mysite/Appointment_Days/admin.py
+++
@@ -1,6 +0,0 @@
-#import webapp2 
-from django.contrib import admin
-#import django.template.loader as dj
-from .models import Appointment
-
-admin.site.register(Appointment)
--- mysite/Appointment_Days/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppointmentDaysConfig(AppConfig):
-    name = 'Appointment_Days'
